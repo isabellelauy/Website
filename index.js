547760380b2f2864932ff4e17a82d5747dbf5fec
Isabelle Lau <isabelleymlau@gmail.com>
2024-06-23 01:29:34 -0400

    bug fixes

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
 });
+
+function scrollToSection() {
+    const section = document.getElementById('done-section');
+    const offset = 80;
+
+    window.scrollTo({
+        top: section.offsetTop - offset,
+        behavior: 'smooth'
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('playButton').addEventListener('click', scrollToSection);
+});
