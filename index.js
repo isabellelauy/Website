78d7d439b6fde7c78173a39b1e508c5499a1ea23
Isabelle Lau <isabelleymlau@gmail.com>
2024-06-23 14:43:35 -0400

    adding the "about" page

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const hamMenu = document.getElementById('ham-menu');
 const offScreenMenu = document.getElementById('off-screen-menu');
+const images = document.querySelectorAll('.main-image');
 
 hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
@@ -18,4 +19,17 @@ function scrollToSection() {
 
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('playButton').addEventListener('click', scrollToSection);
+});
+
+images.forEach(image=> {
+    const originalSrc = image.src;
+    const hoverSrc = image.dataset.hover;
+
+    image.addEventListener('mouseover', () => {
+        image.src=hoverSrc;
+    });
+
+    image.addEventListener('mouseout', () => {
+        image.src=originalSrc;
+    });
 });
